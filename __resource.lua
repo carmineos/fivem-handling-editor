7bc7f067dfdba63071bf7bea1287466df3fbf271
Carmine <neos7@live.it>
2018-06-11 13:06:42 +0200

    Added config.ini, HandlingInfo.xml and __resource.lua to the solution

---
+++ __resource.lua
@@ -0,0 +1,11 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+files {
+	'HandlingInfo.xml',
+	'config.ini'
+}
+client_script {
+	'System.Xml.custom.net.dll',
+	'nativeui.net.dll',
+	'HandlingEditor.net.dll'
+}
