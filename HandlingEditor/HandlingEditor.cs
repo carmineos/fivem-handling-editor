9e200d3385dc10c471678764305a454aa50cb57e
Carmine <neos7@live.it>
2018-07-01 18:34:25 +0200

    Moved KvpList and VehicleList to Utilities.cs

--- HandlingEditor/HandlingEditor.cs
+++ HandlingEditor/HandlingEditor.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Drawing;
 using System.Text;
-using System.Collections;
 using NativeUI;
 using CitizenFX.Core;
 using CitizenFX.Core.UI;
@@ -1465,63 +1464,6 @@ namespace handling_editor
 
                 Debug.WriteLine($"{ScriptName}: Settings {nameof(timer)}={timer} {nameof(debug)}={debug} {nameof(maxSyncDistance)}={maxSyncDistance}");
             }
-        }
-
-    }
-   
-    public class KvpList : IEnumerable<string>
-    {
-        public string prefix;
-
-        public KvpList(string kvpprefix)
-        {
-            this.prefix = kvpprefix;
-        }
-
-        public IEnumerator<string> GetEnumerator()
-        {
-            int handle = StartFindKvp(prefix);
-
-            if (handle != -1)
-            {
-                string kvp;
-                do
-                {
-                    kvp = FindKvp(handle);
-
-                    if (kvp != null)
-                        yield return kvp;
-                }
-                while (kvp != null);
-                EndFindKvp(handle);
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-    }
-
-    public class VehicleList : IEnumerable<int>
-    {
-        public IEnumerator<int> GetEnumerator()
-        {
-            int entity = -1;
-            int handle = FindFirstVehicle(ref entity);
-
-            if (handle != -1)
-            {
-                do yield return entity;
-                while (FindNextVehicle(handle, ref entity));
-
-                EndFindVehicle(handle);
-            }
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
         }
     }
 }
