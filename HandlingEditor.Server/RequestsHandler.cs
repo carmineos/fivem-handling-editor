1525c642bd392f2be9b9b199c2d5dac8463c4870
Carmine <neos7@live.it>
2018-09-13 22:40:49 +0200

    Added server script to manager permissions

---
+++ HandlingEditor.Server/RequestsHandler.cs
@@ -0,0 +1,27 @@
+﻿using CitizenFX.Core;
+using System;
+using static CitizenFX.Core.Native.API;
+
+namespace HandlingEditor.Server
+{
+    public class RequestsHandler : BaseScript
+    {
+        public RequestsHandler()
+        {
+            EventHandlers.Add("HandlingEditor:RequestEditVehiclePermissions", new Action<Player>(SendEditVehiclePermissions));
+            EventHandlers.Add("HandlingEditor:RequestSaveServerPresetPermissions", new Action<Player>(SendSaveServerPresetPermissions));
+        }
+
+        private void SendSaveServerPresetPermissions([FromSource]Player source)
+        {
+            if (IsPlayerAceAllowed(source.Handle, "HandlingEditor.SaveServerPreset"))
+                TriggerClientEvent(source, "HandlingEditor.SaveServerPreset");
+        }
+
+        private void SendEditVehiclePermissions([FromSource]Player source)
+        {
+            if (IsPlayerAceAllowed(source.Handle, "HandlingEditor.EditVehicle"))
+                TriggerClientEvent(source, "HandlingEditor.EditVehicle");
+        }
+    }
+}
