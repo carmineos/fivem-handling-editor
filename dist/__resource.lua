17c27ff3c1c7ca0c65ae16415814fd7e2f908b78
Carmine <neos7@live.it>
2018-09-30 19:48:39 +0200

    nothing important here

--- dist/__resource.lua
+++ dist/__resource.lua
@@ -1,5 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
+--dependency 'NativeUI'
+
 files {
 	'HandlingInfo.xml',
 	'HandlingPresets.xml',
@@ -7,6 +9,7 @@ files {
 	'config.ini'
 }
 client_script {
+	--'@NativeUI/NativeUI.net.dll',
 	'NativeUI.net.dll',
 	'System.Xml.Mono.net.dll',
 	'HandlingEditor.Client.net.dll'
