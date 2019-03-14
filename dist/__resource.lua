7cd4ada071381b1522529efaf5c946608391fcb6
Carmine <neos7@live.it>
2019-03-14 18:30:38 +0100

    Restored System.Xml.Mono as client_script as FiveM won't load it otherwise

--- dist/__resource.lua
+++ dist/__resource.lua
@@ -4,13 +4,13 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 files {
 	--'@MenuAPI/MenuAPI.dll',
-	'MenuAPI.dll',
-	'System.Xml.Mono.dll',
+	'MenuAPI.dll',	
 	'HandlingInfo.xml',
 	'HandlingPresets.xml',
 	'VehiclesPermissions.xml',
 	'config.ini'
 }
 client_script {
+	'System.Xml.Mono.net.dll',
 	'HandlingEditor.Client.net.dll'
 }
