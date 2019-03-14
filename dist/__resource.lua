b7dc2a9ea2c200d9ebaea8be42104b481e622d51
Carmine <neos7@live.it>
2019-03-14 15:20:38 +0100

    Updated client project to use CitizenFX.Core.Client nuget package and updated MenuAPI to the new version

--- dist/__resource.lua
+++ dist/__resource.lua
@@ -3,14 +3,14 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 --dependency 'MenuAPI'
 
 files {
+	--'@MenuAPI/MenuAPI.dll',
+	'MenuAPI.dll',
 	'HandlingInfo.xml',
 	'HandlingPresets.xml',
 	'VehiclesPermissions.xml',
 	'config.ini'
 }
 client_script {
-	--'@MenuAPI/MenuAPI.net.dll',
-	'MenuAPI.net.dll',
 	'System.Xml.Mono.net.dll',
 	'HandlingEditor.Client.net.dll'
 }
