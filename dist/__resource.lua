b0facc491f3086554d5b7f60247c47a1aef2fd17
Carmine <neos7@live.it>
2019-03-14 15:38:45 +0100

    Update reference to System.Xml.Mono and removed useless assemblies replaced by nuget packages

--- dist/__resource.lua
+++ dist/__resource.lua
@@ -5,12 +5,12 @@ resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 files {
 	--'@MenuAPI/MenuAPI.dll',
 	'MenuAPI.dll',
+	'System.Xml.Mono.dll',
 	'HandlingInfo.xml',
 	'HandlingPresets.xml',
 	'VehiclesPermissions.xml',
 	'config.ini'
 }
 client_script {
-	'System.Xml.Mono.net.dll',
 	'HandlingEditor.Client.net.dll'
 }
